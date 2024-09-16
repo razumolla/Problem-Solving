62e109f3d61dd43ea2e76e2947042b1f773e90ac
razumolla <razumolla75@gmail.com>
2024-09-17 00:29:24 +0600

    basic program

---
+++ C Programming/basic.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(){
+
+    for(int i=0;i <=10; i++){
+        printf("This is- %d \n",i);
+    }
+};
--- C Programming/hello.c
+++ C Programming/hello.c
@@ -1,10 +1,5 @@
 #include<stdio.h>
 
 int main(){
-    int num=21;
-   
-
-    for(int i=0;i <=num; i++){
-        printf("This is- %d \n",i);
-    }
+    printf("Hello programming , My name is Razu.");
 };
