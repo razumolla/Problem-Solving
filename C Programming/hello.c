1e3b37b80fad6f1deeb0d93358a1674d8a698fea
razumolla <razumolla75@gmail.com>
2024-09-17 09:01:38 +0600

    data type

--- C Programming/hello.c
+++ C Programming/hello.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
 
 int main(){
-    printf("Hello programming , My name is Razu.");
+    printf("Hello programming\t,My name is Razu.");
 };
---
+++ C Programming/variable.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+
+    float data ; 
+    scanf("%f%%",&data);
+
+    printf("data = %0.2f%% \n",data );
+};
